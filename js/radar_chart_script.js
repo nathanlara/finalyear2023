9e8021c3132b2b6d619bfed800adfcb93b7b0c9c
nathanlara <56224324+nathanlara@users.noreply.github.com>
2023-12-29 12:41:13 +0000

    ajustando radar chart e gerador de pdf

--- js/radar_chart_script.js
+++ js/radar_chart_script.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const answers = JSON.parse(localStorage.getItem('surveyResults'));
 
-    console.log(answers)
-
+    // Definição de likertMapping
     const likertMapping = {
         'Discordo Totalmente': 0,
         'Discordo': 1,
@@ -11,68 +10,101 @@ document.addEventListener('DOMContentLoaded', function() {
         'Concordo Totalmente': 4
     };
 
-    // Array de perguntas - substitua isso pelo seu array real de perguntas
-    const questionsList = [
-        // ... suas perguntas com 'theme', 'id', etc. ...
-    ];
+    fetch('../files/modified_translated_questions.json') // Caminho para o arquivo JSON com as perguntas
+        .then(response => response.json())
+        .then(data => {
+            const questionsList = data.questions;
 
-    // Criar um mapeamento de ID de pergunta para tema
-    const questionThemes = questionsList.reduce((acc, question) => {
-        acc[question.id] = question.theme;
-        return acc;
-    }, {});
+            // Mapear ID da pergunta para o tema
+            const questionThemes = questionsList.reduce((acc, question) => {
+                acc[question.id] = question.theme;
+                return acc;
+            }, {});
 
-    var scoresByTheme = {};
-    var questionCountsByTheme = {};
+            let scoresByTheme = {};
+            let questionCountsByTheme = {};
 
-    for (const questionId in answers) {
-        const response = answers[questionId];
-        const theme = questionThemes[questionId];
-        const score = likertMapping[response];
+            answers.forEach(answer => {
+                const theme = questionThemes[answer.id];
+                const score = likertMapping[answer.response];
 
-        if (!theme || score === undefined) {
-            console.error('Tema ou resposta não encontrados para a pergunta:', questionId);
-            continue;
-        }
+                if (!theme || score === undefined) {
+                    console.error('Tema ou resposta não encontrados para a pergunta:', answer.id);
+                    return;
+                }
 
-        if (!scoresByTheme[theme]) {
-            scoresByTheme[theme] = 0;
-            questionCountsByTheme[theme] = 0;
-        }
+                if (!scoresByTheme[theme]) {
+                    scoresByTheme[theme] = 0;
+                    questionCountsByTheme[theme] = 0;
+                }
 
-        scoresByTheme[theme] += score;
-        questionCountsByTheme[theme]++;
-    }
+                scoresByTheme[theme] += score;
+                questionCountsByTheme[theme]++;
+            });
 
-    const themes = Object.keys(scoresByTheme);
-    const dataPoints = themes.map(theme => {
-        const average = scoresByTheme[theme] / questionCountsByTheme[theme] / 4; // Normalizando a média
-        return average;
-    });
+            const themes = Object.keys(scoresByTheme);
+            const dataPoints = themes.map(theme => {
+                const average = scoresByTheme[theme] / questionCountsByTheme[theme] / 4; // Normalizando a média
+                return average;
+            });
 
-    const ctx = document.getElementById('radarChart').getContext('2d');
-    const radarChart = new Chart(ctx, {
-        type: 'radar',
-        data: {
-            labels: themes,
-            datasets: [{
-                label: 'Resultado do Questionário',
-                data: dataPoints,
-                fill: true,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgb(255, 99, 132)',
-                pointBackgroundColor: 'rgb(255, 99, 132)',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: 'rgb(255, 99, 132)'
-            }]
-        },
-        options: {
-            scale: {
-                ticks: { beginAtZero: true, max: 1 }
-            }
-        }
-    });
+            const ctx = document.getElementById('radarChart').getContext('2d');
+            const radarChart = new Chart(ctx, {
+                type: 'radar',
+                data: {
+                    labels: themes,
+                    datasets: [{
+                        label: 'Meu Círculo da Vida em 2023',
+                        data: dataPoints,
+                        fill: true,
+                        backgroundColor: 'rgba(135, 206, 235, 0.2)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        pointBackgroundColor: 'rgb(255, 99, 132)',
+                        pointBorderColor: '#fff',
+                        pointHoverBackgroundColor: '#fff',
+                        pointHoverBorderColor: 'rgb(255, 99, 132)'
+                    }]
+                },
+                options: {
+                    scale: {
+                        ticks: { beginAtZero: true, max: 1 }
+                    }
+                }
+            });
+        })
+        .catch(error => console.error('Error loading questions:', error));
 });
+
+function generatePDF() {
+    html2canvas(document.querySelector("#radarChart")).then(canvas => {
+        const radarImgData = canvas.toDataURL('image/png');
+
+        const { jsPDF } = window.jspdf;
+        const doc = new jsPDF();
+
+        const logoImg = '../img/logo.png'; 
+        // Adicionar o logotipo: Certifique-se de que o caminho da imagem está correto
+        doc.addImage(logoImg, 'PNG', 10, 10, 10, 10);
+
+        let y = 35; // Ajustar conforme a posição do logotipo
+
+        doc.setFontSize(16);
+        doc.setFont(undefined, 'bold');
+        doc.text(10, y, 'Refletindo sobre o Gráfico de Radar');
+        y += 10;
+
+        // Adicionar o gráfico de radar
+        doc.addImage(radarImgData, 'PNG', 10, y, 180, 180); 
+        y += 110; 
+        
+        doc.save('resultado-grafico-radar.pdf');
+    }).catch(error => {
+        console.error('Erro ao gerar o PDF:', error);
+    });
+}
+
+
+
+
 
 
